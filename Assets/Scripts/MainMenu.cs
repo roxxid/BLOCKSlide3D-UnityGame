6dd74470d0d264de5eae790dc131e346b50b2b4a
Nishant Koli <nishant.coolie@gmail.com>
2017-01-05 18:11:22 +0530

    Add files via upload

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public GUISkin skin;
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		GUI.Label (new Rect(10,10, 400,75), "Go Home");
+		if (PlayerPrefs.GetInt("Level Completed") > 1)
+		{
+			if (GUI.Button(new Rect(10,100,100,45), "Continue"))
+			{
+				Application.LoadLevel(PlayerPrefs.GetInt("Level Completed"));
+			}
+		}
+		if (GUI.Button(new Rect(10,155,100,45), "New Game"))
+		{
+			PlayerPrefs.SetInt("Level Completed", 1);
+			Application.LoadLevel(0);
+		}
+		if (GUI.Button(new Rect(10,210,100,45), "Quit"))
+		{
+			Application.Quit ();
+		}
+
+	}
+}
